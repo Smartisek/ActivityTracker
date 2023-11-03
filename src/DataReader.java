ac5eed9289e08ecd5dbbb74eeb98a354df6e9281
Smartisek <domalipdominik@gmail.com>
2023-11-03 19:23:31 +0000

    adding calories

--- src/DataReader.java
+++ src/DataReader.java
@@ -27,6 +27,7 @@ public class DataReader{
     public double getDuration() {return duration;}
     public double getDistance() {return distance;}
     public int getHeartRate() {return heartRate;}
+
     public void setCalories(double calories){ this.calories = calories;}
 
     public double getSpeed(){
@@ -43,7 +44,8 @@ public class DataReader{
                 ", distance=" + distance +
                 ", heartRate=" + heartRate +
                 ", Speed=" + String.format("%.1f",getSpeed()) + "km/h" +
-                ", Intensity= " + CheckIntensity() +
+                ", Intensity= " +  CheckIntensity() +
+                ", Calories= " + String.format("%.1f", calories) + " kcal" +
                 '}' +"\n";
     }
 
@@ -53,51 +55,59 @@ public class DataReader{
             if(this.getSpeed() <= 0.5){
 //                this.setCalories(this.distance * 5);
 //                System.out.println("Very Light " + calories);
+                this.setCalories(5*(this.duration));
                return intensity[0];
             } else if(this.getSpeed() >0.5 && this.getSpeed() <= 1.25){
+                this.setCalories(6.3*(this.duration));
                 return intensity[1];
             } else if(this.getSpeed() >1.25 && this.getSpeed() <=2.00){
+                this.setCalories(7.6*(this.duration));
                 return intensity[2];
             } else if(this.getSpeed() >2.00 && this.getSpeed() <= 2.75){
+                this.setCalories(8.9*(this.duration));
                 return intensity[3];
             } else if(this.getSpeed() >2.75){
+                this.setCalories(10.2*(this.duration));
                 return intensity[4];
             }
         }
         if(this.getActivity().equals("Running")){
             if(this.getSpeed() < 4.00){
+                this.setCalories(4.1*(this.duration));
                 return intensity[0];
             } else if(this.getSpeed() >= 4.00 && this.getSpeed() <8.00){
+                this.setCalories(7.2*(this.duration));
                 return intensity[1];
             } else if(this.getSpeed() >= 8.00 && this.getSpeed() < 12.00){
+                this.setCalories(10*(this.duration));
                return intensity[2];
             } else if(this.getSpeed() >=12.00 && this.getSpeed() < 16.00){
+                this.setCalories(15.4*(this.duration));
                 return intensity[3];
             } else if(this.getSpeed() >= 16.00){
+                this.setCalories(20.8*(this.duration));
                 return intensity[4];
             }
         }
         if(this.getActivity().equals("Cycling")){
             if(this.getSpeed() < 8.00){
+                this.setCalories(2*(this.duration));
                 return intensity[0];
             } else if(this.getSpeed() >= 8.00 && this.getSpeed() <= 16.00){
+                this.setCalories(5*(this.duration));
                 return intensity[1];
             } else if(this.getSpeed() >= 17.00 && this.getSpeed() < 25.00 ){
+                this.setCalories(7*(this.duration));
                 return intensity[2];
             } else if(this.getSpeed() >= 25.00 && this.getSpeed() <33.00){
+                this.setCalories(13*(this.duration));
                 return intensity[3];
             } else if(this.getSpeed() >= 33.00){
+                this.setCalories(15*(this.duration));
                 return intensity[4];
             }
         }
         return "!=intensity";
     }
 
-//    public void GetSwimmingIntensity(){
-//        System.out.println(this.getActivity());
-//        if(this.getActivity().equals("Swimming")){
-//            System.out.println("swim");
-//        }
-//
-//    }
 }
